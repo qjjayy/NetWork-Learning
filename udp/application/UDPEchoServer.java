11ec7ba10e5bfa47bf6830f8ab5a9074c4d1186d
qjj <xiaohaixie@qq.com>
2017-06-29 21:41:13 +0800

    commit-echoserver

---
+++ udp/application/UDPEchoServer.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPEchoServer extends UDPServer {
+
+	public final static int DEFAULT_PORT = 1024;
+
+	public UDPEchoServer() {
+		super(DEFAULT_PORT);
+	}
+
+	@Override
+	public void respond(DatagramSocket socket, DatagramPacket request) throws IOException {
+		DatagramPacket outgoing = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(),
+				request.getPort());
+		socket.send(outgoing);
+	}
+
+	public static void main(String[] args) {
+		UDPEchoServer server = new UDPEchoServer();
+		Thread t = new Thread(server);
+		t.start();
+	}
+
+}
